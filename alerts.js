72d6abfd4ba7a3e2ed53363d6a2f59e122b29e2d
Mateus <mmreis@rd.com.br>
2022-01-18 09:49:41 -0300

    Atualização da pagina de 'Meu perfil' e 'Entrega'

---
+++ alerts.js
@@ -0,0 +1,36 @@
+      function funcao1() {
+      alert("Formulario Enviado Com Sucesso!");
+      }
+      // Pagina Fale conosco 
+      function funcao2()
+      {
+      alert("Mensagem enviada com sucesso.");
+      }
+      function funcao3()
+      {
+      alert("Email Enviado , Verifique sua Caixa de Email.");
+      }
+      function funcao4()
+      {
+      alert("Cadastro Realizado com Sucesso");
+      }
+      function funcao5()
+      {
+      alert("Avaliação Enviada com sucesso.");
+      }
+      function funcao6()
+      {
+          alert("Deseja Finalizar a compra?")
+      }
+      function funcao7()
+      {
+        var x;
+        var r=confirm("Deseja realmente excluir esse cartão?");
+        }
+    function funcao8()
+        {
+        var x;
+        var r=confirm("Deseja realmente excluir esse endereço?");
+        }
+
+      // <script type="text/javascript" src="../paginas/alerts.js"></script> //
